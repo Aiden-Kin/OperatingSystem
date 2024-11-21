bcf7174d2e29056ffbce0bcd367eaef6797c3012
1linkoff <1472526789@qq.com>
2024-11-21 16:33:45 +0800

    测试

---
+++ OperatingSystem/测试.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatingSystem
+{
+    internal class 测试
+    {
+        string a = "测试同步；";
+    }
+}
